981e15b4efa9e3a1ce6badac3815e45784f4cb70
Ngô Lê Tuyết Hoa <80042406+tuyethoa1011@users.noreply.github.com>
2022-12-04 08:33:53 +0700

    Update countdown

--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,66 @@ function show_clock()
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};  
     return i;
-  }
+}
+
+function change_page()
+{
+    window.location.href = "count_down.html";
+}
+
+
+
+const countdown = 
+{
+    h: null,
+    s: null,
+    m: null,
+    timeout: null
+}
+
+
+function start()
+{   
+    if(countdown.h == null)
+        {
+            countdown.h = parseInt(document.getElementById("gio").value);
+            countdown.m = parseInt(document.getElementById("phut").value);
+            countdown.s = parseInt(document.getElementById("giay").value);
+        }
+
+        if(countdown.s === -1){
+            countdown.m = countdown.m - 1;
+            countdown.s = 59;
+        }
+
+        if(countdown.m === -1){
+            countdown.h = countdown.h - 1;
+            countdown.m = 59;
+        }
+
+        if(countdown.h === -1){
+            clearTimeout(countdown.timeout);
+            window.alert("Hết giờ")
+            return false;
+        }
+
+        //Display time
+        document.getElementById("h").innerText = checkTime(countdown.h.toString());
+        document.getElementById("m").innerText = checkTime(countdown.m.toString());
+        document.getElementById("s").innerText = checkTime(countdown.s.toString());
+
+        //decre secs then call function again after 1 sec
+        countdown.timeout = setTimeout( function(){
+            countdown.s--;
+            start();
+        },1000);
+}
+
+function stop()
+{
+    clearTimeout(countdown.timeout);
+}
+
+
